a34f4fef8c93cf038a5665926d0b09184c81072d
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-05 12:38:06 -0600

    removed logger and built initial favretweet.py bot

--- bots/config.py
+++ bots/config.py
@@ -1,9 +1,6 @@
 # a resuable module containing the logic common to all bots
 import tweepy
-import logging
 import os
-
-logger = logging.getLogger()
 
 
 def create_api():
@@ -11,17 +8,17 @@ def create_api():
     consumer_secret = os.getenv("CONSUMER_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
+
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
     try:
         api.verify_credentials()
-    except Exception as e:
-        logger.error('Error creating API', exc_info=True)
+    except:
+        print('Error creating API')
 
-        raise e
-
-    logger.info('API was created successfully')
+    print('API was created successfully')
 
     return api
--- bots/favretweet.py
+++ bots/favretweet.py
@@ -1,1 +1,22 @@
 # automatically likes and retweets tweets that match a certain criteria
+import tweepy
+from config import create_api
+
+
+class FavRetweetListener(tweepy.Stream):
+    def on_status(self, status):
+        print(status.text)
+
+
+def main(keywords):
+    api = create_api()
+    tweet_listener = FavRetweetListener(
+        api.auth.consumer_key, api.auth.consumer_secret,
+        api.auth.access_token, api.auth.access_token_secret
+    )
+
+    tweet_listener.filter(track=keywords, languages=["en"])
+
+
+if __name__ == '__main__':
+    main(["Python"])
