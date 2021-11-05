0fc2ca77d6c4b41206b8c6fdb586af93a271f525
Logan Kimball <43894929+logankimbs@users.noreply.github.com>
2021-11-05 10:37:11 -0600

    built followfollowers.py bot

--- bots/followfollowers.py
+++ bots/followfollowers.py
@@ -1,1 +1,25 @@
 # automatically follows anyone who follows me
+import logging
+from config import create_api
+import time
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def follow_followers(api):
+    logger.info('Retrieving and following followers')
+    print(api)
+
+
+def main():
+    api = create_api()
+
+    while True:
+        follow_followers(api)
+        logger.info('Waiting for accounts to follow...')
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+    main()
